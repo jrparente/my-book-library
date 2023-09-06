abda032ae5211e98e420d170666a07d40ac96240
Joana Parente <jrparente@gmail.com>
2023-09-06 17:12:39 +0100

    .

--- src/components/Dashboard/DeleteModal/DeleteModal.js
+++ src/components/Dashboard/DeleteModal/DeleteModal.js
@@ -45,7 +45,7 @@ const DeleteModal = ({ showModal, handleDelete, handleClose }) => {
               className="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900"
               onClick={handleDelete}
             >
-              Yes, I'm sure
+              Yes, I&lsquo;m sure
             </button>
           </div>
         </div>
